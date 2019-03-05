988bb13cadbda18f13484c1c069ad84725c1b86c
CarsonBain <carsonbain@gmail.com>
2019-03-05 11:32:59 -0800

    move image to import in index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 require('./scss/main.scss');
+import img from '../src/images/thumb.png';
 console.log('Webpack is working!');
+document.querySelector(
+  '.image'
+).innerHTML = `<img src='${img}' alt='Thumbs up' />`;
